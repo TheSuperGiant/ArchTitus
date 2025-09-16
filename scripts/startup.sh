153a6b66ff9c7024f8adc6bb17e73b9e6b2b6514
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-09-16 13:43:16 +0000

    Add files via upload

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -276,8 +276,8 @@ case ${options[$?]} in
 			if [ "$continent" = "$item" ]; then
 				echo -e "good\n\n"
 				#echo "options_$continent"
+				select_option $? 4 "${options_$continent[@]}"
 				case ${options_$continent[$?]} in
-					select_option $? 4 "${options_$continent[@]}"
 					echo "$continent\options_$continent[$?]"
 					read -p "Press [Enter] to continue..."
 					#"$continent\$city"
